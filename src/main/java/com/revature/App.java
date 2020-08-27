c506c2d5b2abacf91a0a8aa8f78a4bbd2397587c
Mehrab Rahman <mehrab.rahman@revature.com>
2020-08-27 03:27:51 -0500

    Fixed Tomcat embed

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,22 +1,29 @@
 package com.revature;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.File;
 import java.util.Optional;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.WebResourceSet;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 
 public class App {
     public static final Optional<String> port = Optional.ofNullable(System.getenv("PORT"));
 
     public static void main(String[] args) throws Exception {
-        // String contextPath = "/monitor";
-        // String appBase = ".";
-        // Tomcat tomcat = new Tomcat();
-        // tomcat.getConnector();
-        // tomcat.setPort(Integer.valueOf(port.orElse("8080")));
-        // tomcat.getHost().setAppBase(appBase);
-        // tomcat.addWebapp(contextPath, appBase);
-        // tomcat.start();
-        // tomcat.getServer().await();
-
+        final String base = new File("./").getAbsolutePath();
+        Tomcat server = new Tomcat();
+        server.setPort(Integer.valueOf(port.orElse("8080")));
+        server.getConnector();
+        Context context = server.addWebapp("", base);
+        WebResourceRoot resources = new StandardRoot(context);
+        WebResourceSet webResourceSet = new DirResourceSet(resources, "/WEB-INF/classes", base, "/");
+        resources.addPreResources(webResourceSet);
+        context.setResources(resources);
+        server.start();
+        server.getServer().await();
     }
 }
